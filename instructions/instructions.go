1a549ed098d274f0424ce54ee8d3479e140747fe
chdorner <christof@chdorner.com>
2013-08-04 00:08:44 +0200

    Validate instructions

--- instructions/instructions.go
+++ instructions/instructions.go
@@ -29,8 +29,30 @@ func ParseInstructions(p url.Values) (*RequestInstructions, error) {
 
 	instr.Height, err = strconv.Atoi(p.Get("height"))
 	if err != nil {
-		return nil, errors.New("height is not an integer")
+		instr.Height = 0
+	}
+
+	err = instr.validate()
+	if err != nil {
+		return nil, err
 	}
 
 	return instr, nil
+}
+
+func (i *RequestInstructions) validate() error {
+	if i.Action == "crop" && (i.Width <= 0 || i.Height <= 0) {
+		return errors.New("both width and height need to be bigger than 0 with crop action")
+	}
+
+	if i.Format != "png" && i.Format != "jpg" {
+		return errors.New("format needs to be either jpg or png")
+	}
+
+	originUrl, err := url.ParseRequestURI(i.Origin)
+	if err != nil || originUrl.Host == "" || originUrl.Scheme == ""{
+		return errors.New("origin is not a valid url")
+	}
+
+	return nil
 }
--- instructions/instructions_test.go
+++ instructions/instructions_test.go
@@ -41,3 +41,116 @@ func TestParseInstrunctions(t *testing.T) {
 		t.Fatalf("height is not as expected, got: %d", instructions.Height)
 	}
 }
+
+func TestParseInstructionsCropWithoutWidth(t *testing.T) {
+	queryString := "origin=http%3A%2F%2Fexample.com%2Ftest.jpg" +
+		"&action=crop" +
+		"&format=png" +
+		"&height=200"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err == nil {
+		t.Fatal("instructions should not be valid")
+	}
+}
+
+func TestParseInstructionsCropWithoutHeight(t *testing.T) {
+	queryString := "origin=http%3A%2F%2Fexample.com%2Ftest.jpg" +
+		"&action=crop" +
+		"&format=png" +
+		"&width=200"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err == nil {
+		t.Fatal("instructions should not be valid")
+	}
+}
+
+func TestParseInstructionsShrink(t *testing.T) {
+	queryString := "origin=http%3A%2F%2Fexample.com%2Ftest.jpg" +
+		"&action=shrink-w" +
+		"&format=png" +
+		"&width=200"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParseInstructionsShrinkWithoutWidth(t *testing.T) {
+	queryString := "origin=http%3A%2F%2Fexample.com%2Ftest.jpg" +
+		"&action=shrink-w" +
+		"&format=png"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err == nil {
+		t.Fatal("instructions should not be valid")
+	}
+}
+
+func TestParseInstructionsWithoutFormat(t *testing.T) {
+	queryString := "origin=http%3A%2F%2Fexample.com%2Ftest.jpg" +
+		"&action=crop" +
+		"&width=200" +
+		"&height=200"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err == nil {
+		t.Fatal("instructions should not be valid")
+	}
+}
+
+func TestParseInstructionsWithInvalidFormat(t *testing.T) {
+	queryString := "origin=http%3A%2F%2Fexample.com%2Ftest.jpg" +
+		"&action=crop" +
+		"&width=200" +
+		"&height=200" +
+		"&format=gif"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err == nil {
+		t.Fatal("instructions should not be valid")
+	}
+}
+
+func TestParseInstructionsWithInvalidOrigin(t *testing.T) {
+	queryString := "origin=%2Fimage.png" +
+		"&action=crop" +
+		"&width=200" +
+		"&height=200" +
+		"&format=png"
+	query, err := url.ParseQuery(queryString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ParseInstructions(query)
+	if err == nil {
+		t.Fatal("instructions should not be valid")
+	}
+}
