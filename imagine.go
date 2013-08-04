057ad5e42d592c65b972394dab4335a7dae1073c
chdorner <christof@chdorner.com>
2013-08-04 12:57:20 +0200

    Parse origin whitelist flags as slice of regexps

--- imagine.go
+++ imagine.go
@@ -8,17 +8,24 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"regexp"
+	"strings"
 	"syscall"
 )
 
 var (
-	addr    = flag.String("a", ":8080", "address to bind to")
-	daddr   = flag.String("debug.a", ":8081", "address to bind to for debug information")
-	version = flag.Bool("version", false, "print version and exit")
+	addr       string
+	daddr      string
+	version    bool
+	oWhitelist originWhitelist
 )
 
+type originWhitelist struct {
+	slice []*regexp.Regexp
+}
+
 func main() {
-	if *version {
+	if version {
 		fmt.Println(server.Version)
 		os.Exit(0)
 	}
@@ -26,7 +33,7 @@ func main() {
 	go handleSignals(sigch)
 	signal.Notify(sigch)
 
-	log.Fatal(server.ListenAndServe(*addr))
+	log.Fatal(server.ListenAndServe(addr))
 }
 
 func handleSignals(sigch chan os.Signal) {
@@ -41,5 +48,28 @@ func handleSignals(sigch chan os.Signal) {
 }
 
 func init() {
+	flag.StringVar(&addr, "a", ":8080", "address to bind to")
+	flag.StringVar(&daddr, "debug.a", ":8081", "address to bind to for debug information")
+	flag.BoolVar(&version, "version", false, "print version and exit")
+	flag.Var(&oWhitelist, "o", "origin whitelist regex (may be used multiple times)")
 	flag.Parse()
+}
+
+func (w *originWhitelist) Set(s string) error {
+	r, err := regexp.Compile(s)
+	if err != nil {
+		return fmt.Errorf(`"%s" is not a valid regular expression`, s)
+	}
+
+	w.slice = append(w.slice, r)
+	return nil
+}
+
+func (w *originWhitelist) String() string {
+	ret := make([]string, len(w.slice))
+	for i, r := range w.slice {
+		ret[i] = fmt.Sprint(r.String())
+	}
+	str := fmt.Sprintf("[%s]", strings.Join(ret, " "))
+	return str
 }
